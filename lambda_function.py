c89ac8df03178f4654304fc304c90cadde1a4138
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2021-10-11 20:29:50 -0500

    Update and rename lambda_function1.py to lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 def lambda_handler(event, context):
     # TODO implement
+    #run this
     image = event['data']
     image = numpy.array(Image.open(BytesIO(image))).astype(numpy.float32)
     return {
